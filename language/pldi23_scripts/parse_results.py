1c015ac1752dcdca50b9640e5a88393ed33e0442
Anjiang-Wei <1020681930@qq.com>
2022-10-25 22:09:55 -0700

    improve parsing script

--- language/pldi23_scripts/parse_results.py
+++ language/pldi23_scripts/parse_results.py
@@ -22,15 +22,25 @@ def parse_content(path):
             return ('ERROR',)
         return match.groups()
 
+# GFLOPS = 485417.560 GFLOPS
+_content_re2 = re.compile(r'^GFLOPS = +([0-9.]+) GFLOPS$', re.MULTILINE)
+def parse_content2(path):
+    with open(path, 'r') as f:
+        content = f.read()
+        match = re.search(_content_re2, content)
+        if match is None:
+            return ('ERROR',)
+        return match.groups()
+
 def main():
     paths = glob.glob('*/*.log')
-    content = [(os.path.dirname(path),) + parse_basename(os.path.basename(path)) + parse_content(path) for path in paths]
+    content = [(os.path.dirname(path),) + parse_basename(os.path.basename(path)) + parse_content(path) + parse_content2(path) for path in paths]
     content.sort(key=lambda row: (row[0], int(row[1]), int(row[2]), int(row[3])))
 
     import sys
     # with open(out_filename, 'w') as f:
-    out = csv.writer(sys.stdout, dialect='excel-tab') # f)
-    out.writerow(['system', 'nodes', 'procs_per_node', 'rep', 'elapsed_time'])
+    out = csv.writer(sys.stdout)# , dialect='excel-tab') # f)
+    out.writerow(['system', 'nodes', 'procs_per_node', 'rep', 'elapsed_time', 'gflops'])
     out.writerows(content)
 
 if __name__ == '__main__':
