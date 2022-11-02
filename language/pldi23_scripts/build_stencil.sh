4d983a95977d8dee5618497f29ff0fcdb9927836
Anjiang-Wei <1020681930@qq.com>
2022-11-02 09:26:08 -0700

    don't increase problem size by 2 for stencil

--- language/pldi23_scripts/build_stencil.sh
+++ language/pldi23_scripts/build_stencil.sh
@@ -6,7 +6,7 @@ mkdir "$1"
 cd "$1"
 
 # Hack: -ffuture 0 is a workaround for blocking on a future with the trace loop
-build_option="-fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fcuda-arch volta -findex-launch 1 -ffuture 0"
+build_option="-fflow 0 -fopenmp 0 -foverride-demand-cuda 1 -fcuda 1 -fcuda-offline 1 -fgpu-arch volta -findex-launch 1 -ffuture 0"
 SAVEOBJ=1 STANDALONE=1 OBJNAME=./stencil $root_dir/../regent.py $root_dir/../examples/stencil_fast.rg $build_option
 
 cp -v $root_dir/*stencil* .
