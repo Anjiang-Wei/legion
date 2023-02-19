4522fdd82b24c627ece955df7f152561446641bb
Anjiang-Wei <1020681930@qq.com>
2023-02-18 21:24:01 -0800

    add launching script

---
+++ language/pldi23_scripts/build_cholesky_sapling.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+root_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+mkdir "$1"
+cd "$1"
+
+# Hack: -ffuture 0 is a workaround for blocking on a future with the trace loop
+build_option="-fflow 0 -fopenmp 0 -fcuda 0 -findex-launch 1"
+SAVEOBJ=1 STANDALONE=1 OBJNAME=./cholesky $root_dir/../regent.py $root_dir/../examples/cholesky.rg $build_option
+
+cp -v $root_dir/*cholesky* .
+echo "build_cholesky.sh done"
