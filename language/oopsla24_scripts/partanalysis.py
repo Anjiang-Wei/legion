6b0471f69f75c529c7f92523dcbd04239c1c13a8
Anjiang-Wei <1020681930@qq.com>
2024-01-17 23:05:27 -0800

    update analysis scripg

--- language/oopsla24_scripts/partanalysis.py
+++ language/oopsla24_scripts/partanalysis.py
@@ -7,6 +7,7 @@ import glob
 import os
 import re
 import sys
+import pprint
 
 # part_1_16/out_1_16_1_16_r5.log
 
@@ -14,9 +15,9 @@ _filename_re = re.compile(r'out_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+)_r([0-9]+)[.]
 def parse_basename(filename):
     match = re.match(_filename_re, filename)
     assert match is not None
-    assert len(match.groups) == 5
+    assert len(match.groups()) == 5
     # NX, NY, NTX, NTY, repetition
-    return list(map(int, match.groups()))
+    return tuple(map(int, match.groups()))
 
 _content_re = re.compile(r'^ELAPSED TIME = +([0-9.]+) s$', re.MULTILINE)
 def parse_content(path):
@@ -26,24 +27,32 @@ def parse_content(path):
         if match is None:
             print(f"Error parsing {path}")
             exit(1)
-        assert len(match.groups) == 1
+        assert len(match.groups()) == 1
         return float(match.groups()[0])
 
 def compute_average(content):
-    pass
+    for key in content:
+        content[key] = sum(content[key]) / len(content[key])
+    # sort the result by key[0]*key[1] (total GPU numbers), key[0] (NX)
+    new_content = {key: val for key, val in sorted(content.items(), key = lambda x: (x[0][0] * x[0][1], x[0][0]))}
+    res = []
+    for k in new_content.keys():
+        res.append(list(k) + [new_content[k]])
+    return res
     
 def main():
     paths = glob.glob('*/*.log')
-    content = [parse_basename(os.path.basename(path)) + parse_content(path) for path in paths]
-    print(content)
-    # content.sort(key=lambda row: (row[0], int(row[1]), int(row[2]), int(row[3])))
-
-    # average = compute_average(content)
-    # # with open(out_filename, 'w') as f:
-    # out = csv.writer(sys.stdout)# , dialect='excel-tab') # f)
-    # # out.writerow(['system', 'nodes', 'procs_per_node', 'rep', 'elapsed_time', 'gflops'])
-    # out.writerow(['system', 'nodes', 'procs_per_node', 'rep', 'average_time'])
-    # out.writerows(average)
+    res = {}
+    for path in paths:
+        if parse_basename(os.path.basename(path)) not in res:
+            res[parse_basename(os.path.basename(path))] = []
+        res[parse_basename(os.path.basename(path))].append(parse_content(path))
+    avg = compute_average(res)
+    pprint.pprint(avg)
+    
+    out = csv.writer(sys.stdout)
+    out.writerow(['NX', 'NY', 'NTX', 'NTY', 'time'])
+    out.writerows(avg)
 
 if __name__ == '__main__':
     main()
