6b2e9b27bfeb881fce0bc05d6428a0013c404211
Anjiang-Wei <1020681930@qq.com>
2024-02-05 14:10:23 -0800

    csv gen

--- language/oopsla24_scripts/csvgen.py
+++ language/oopsla24_scripts/csvgen.py
@@ -11,19 +11,20 @@ import pprint
 from statistics import median
 
 repeat = 20
-# all_2_1_256_1767   /out_1_256_1767_1_8_o_1_r20.log
-# all_node_dx_dy_tile/out_dx_dy_tile_px_py_{c|o}_{1|2}_r{1-20}.log
+# swp_2_0_0/out_2_0_0_c_1_1000000_2000000_1414_1415_4_2_r1.log
+# node, tileidx, ratioidx, c_o, dim, tilestart, tilecurrent, domainx, domainy, partx, party, repetition
 
-_filename_re = re.compile(r'all_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+)/out_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+)_(c|o)_(1|2)_r([0-9]+)[.]log')
+_filename_re = re.compile(r'swp_([0-9]+)_([0-9]+)_([0-9]+)/out_([0-9]+)_([0-9]+)_([0-9]+)_(c|o)_(1|2)_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+)_([0-9]+)_r([0-9]+)[.]log')
 def parse_basename(filename):
     match = re.match(_filename_re, filename)
     assert match is not None, filename
-    assert len(match.groups()) == 12
-    node, dx, dy, tile, dx_, dy_, tile_, px, py, c_o, dim, repetition = match.groups()
-    assert dx == dx_ and dy == dy_ and tile == tile_, f"{node} mismatches for {dx} {dy} {tile} {dx_} {dy_} {tile_}"
-    node, dx, dy, tile, px, py, dim, repetition = list(map(int, (node, dx, dy, tile, px, py, dim, repetition)))
-    assert node * 4 == px * py
-    return (node, dx, dy, tile, px, py, c_o, dim, repetition)
+    assert len(match.groups()) == 15
+    node, tileidx, ratioidx, node_, tileidx_, ratioidx_, c_o, dim, tilestart, tilecurrent, domainx, domainy, partx, party, repetition = match.groups()
+    assert node == node_ and tileidx == tileidx_ and ratioidx == ratioidx_, filename
+    node, tileidx, ratioidx, c_o, dim, tilestart, tilecurrent, domainx, domainy, partx, party, repetition = list(map(int, \
+    (node, tileidx, ratioidx, c_o, dim, tilestart, tilecurrent, domainx, domainy, partx, party, repetition)))
+    assert node * 4 == px * py, filename
+    return (node, tileidx, ratioidx, c_o, dim, tilestart, tilecurrent, domainx, domainy, partx, party, repetition)
 
 _content_re = re.compile(r'^ELAPSED TIME = +([0-9.]+) s$', re.MULTILINE)
 def parse_content(path):
@@ -41,7 +42,7 @@ def compute_average(content):
         if len(content[key]) != repeat:
             print(f"key = {key}, != repeat = {repeat}")
         content[key] = median(content[key])
-    # sort the result by node number, then domain_x
+    # sort the result by node number, then tileidx
     new_content = {key: val for key, val in sorted(content.items(), key = lambda x: (x[0][0], x[0][1]))}
     res = []
     for k in new_content.keys():
@@ -61,7 +62,7 @@ def main():
     # pprint.pprint(avg)
     
     out = csv.writer(open("all.csv", "w"))
-    out.writerow(['node', 'dx', 'dy', 'tile', 'px', 'py', 'c_o', 'dim', 'time'])
+    out.writerow(['node', 'tileidx', 'ratioidx', 'c_o', 'dim', 'tilestart', 'tilecurrent', 'domainx', 'domainy', 'partx', 'party', 'time'])
     out.writerows(avg)
 
 if __name__ == '__main__':
